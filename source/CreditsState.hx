39cd49d7fcf3e030353cebc0ef6666ccb0dd0bd6
Dark <94803488+darknooblol@users.noreply.github.com>
2021-12-12 15:15:34 -0300

    Add files via upload

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -46,7 +46,9 @@ class CreditsState extends MusicBeatState
 		add(grpOptions);
 
 		var pisspoop = [ //Name - Icon name - Description - Link - BG Color
-			['Android Port'],
+			['Main leader of the mod'],
+			['Dark_The_Noob',		    'darkthenoob',		    'Did Everything',	 'https://www.youtube.com/channel/UCxQTnLmv0OAS63yzk9pVfaw',	'0xFFFFDD33'],
+            ['Android Port'],
 			['M.A. Jigsaw',		    'majigsaw',		    'Main Coder of The Port',	 'https://www.youtube.com/channel/UC2Sk7vtPzOvbVzdVTWrribQ',	'0xFFC30085'],
 			['Psych Engine Team'],
 			['Shadow Mario',		'shadowmario',		'Main Programmer of Psych Engine',					'https://twitter.com/Shadow_Mario_',	'0xFFFFDD33'],
